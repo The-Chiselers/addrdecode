f7bbe54452c59c0d041863b9b5d3592c416adf21
Nick Allison <nickrallison1@gmail.com>
2024-12-15 14:25:12 -0700

    cleaned

--- src/main/scala/tech/rocksavage/synth/SynthResult.scala
+++
@@ -1,9 +0,0 @@
-// (c) 2024 Rocksavage Technology, Inc.
-// This code is licensed under the Apache Software License 2.0 (see LICENSE.MD)
-
-package tech.rocksavage.synth
-
-class SynthResult(synthString: String, stdout: String) {
-  def getSynthString: String = synthString
-  def getStdout: String = stdout
-}
